056ada02b496a68639ccdd932dfb824947f92896
Anderson Vargas <andersomrobertomachadodevargas@gmail.com>
2024-02-01 19:02:09 -0300

    exemplo de como salvar objeto em xml

---
+++ Teste xml/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+public class clsPerson
+{
+    public string FirstName;
+    public string MI;
+    public string LastName;
+}
+
+class class1
+{
+    static void Main(string[] args)
+    {
+        clsPerson p = new clsPerson();
+        p.FirstName = "Beef Back to the future";
+        p.MI = "A";
+        p.LastName = "Price";
+
+      
+
+        XmlSerializer mySerializer = new XmlSerializer(typeof(clsPerson));
+        // To write to a file, create a StreamWriter object.  
+        StreamWriter myWriter = new StreamWriter(@"C:\Users\Usuario\Documents\Videos\myFileName.xml");
+        mySerializer.Serialize(myWriter, p);
+        myWriter.Close();
+    }
+}
